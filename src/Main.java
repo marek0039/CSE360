0bdaf6c97a16048f459109ba67802c35c33150e5
sydneywilcox <sydneywilcox@macbook-pro-6.3521.dhcp.asu.edu>
2021-10-19 18:05:17 -0700

    This is the home screen GUI
    I'll definitely play around more with the margins/padding and I need to
    figure out the best way to add a background photo, I might post them
    online and use the URL so they are accessible to anyone who runs this
    code.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
-public class Main {
+import javafx.scene.image.*
+import javafx.scene.shape.*
+import javafx.scene.effect.*
+import javafx.scene.paint.*
+import javafx.scene.text.*
+import javafx.scene.control.*
+import java.lang.*
+import javafx.scene.layout.*
+
+public class Main 
+{
     // This will be our Main class with all of our GUI's
+	
+}
+
+//Class for the Home Screen GUI, this is the only background photo that is different
+//as it has the cartoon in the bottom corner
+public class HomeScreen
+{
+	StackPane homePane = new StackPane();
+	
+	
 }
